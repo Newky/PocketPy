df61dcad5d43ee47054fad6c722d10836d1504a6
Newky <richdel1991@gmail.com>
2012-12-28 16:12:53 +0000

    Started on a little abstraction class for loading json config files.

---
+++ jsonconfig.py
@@ -0,0 +1,19 @@
+import json
+
+
+class JsonConfig:
+    def __init__(self, fname, config=None):
+        self.fname = fname
+        self.config = config
+
+    def read(self):
+        if self.config:
+            return self.config
+        with open(self.fname, "rb") as infile:
+            config = json.load(infile)
+            self.config = config
+            return self.config
+
+    def save(self):
+        with open(self.fname, "wb") as outfile:
+            json.dump(self.config, outfile)
