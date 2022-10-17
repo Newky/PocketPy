e68f028e372b294f1d8ae8eba38b3aa24e5e14dd
David V. Lu <davidvlu@gmail.com>
2022-10-17 14:07:10 -0400

    Remove dependencies

--- setup.py
+++ setup.py
@@ -6,14 +6,12 @@ setup(
     author='Richard Delaney',
     author_email='richdel1991@gmail.com',
     packages=['pocketpy', 'pocketpy.test'],
-    scripts=['bin/comments_apply.py', 'bin/operators.py', 'bin/pocket_ticker.py'],
+    scripts=['bin/operators.py', 'bin/pocket_ticker.py'],
     url='http://pypi.python.org/pypi/PocketPy',
     license='LICENSE.txt',
     description='A python wrapper and some scripts for communication with pocket api',
     long_description=open('README.txt').read(),
     install_requires=[
-        "HNComments==0.1.1",
-        "argparse==1.2.1",
         "requests==2.18",
     ],
 )
